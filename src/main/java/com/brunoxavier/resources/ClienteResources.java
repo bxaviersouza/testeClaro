856463faecc35ab827985358d18f4b1e92b9c8fc
Bruno Xavier <b.xavier.souza@gmail.com>
2023-09-16 15:52:42 -0300

    Testando a classe de dominio

--- src/main/java/com/brunoxavier/resources/ClienteResources.java
+++ src/main/java/com/brunoxavier/resources/ClienteResources.java
@@ -1,15 +1,27 @@
 package com.brunoxavier.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brunoxavier.domain.Cliente;
 
 @RestController
 @RequestMapping(value="/clientes")
 public class ClienteResources {
 
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Cliente> listar() {
+		Cliente c1 = new Cliente(1, "Bruno");
+		Cliente c2 = new Cliente(2, "Caio");
+		
+		List<Cliente> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 }
