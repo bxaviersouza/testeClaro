c102656069b68852d54fb8c5e7dc96eaad4ecefa
Bruno Xavier <b.xavier.souza@gmail.com>
2023-09-16 16:22:31 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/brunoxavier/domain/Cliente.java
+++ src/main/java/com/brunoxavier/domain/Cliente.java
@@ -3,10 +3,19 @@ package com.brunoxavier.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Cliente implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
