f01013094f0b22fe3c217b743d22e5b684a1e37a
Bruno Xavier <b.xavier.souza@gmail.com>
2023-09-16 15:10:13 -0300

    Criação do Projeto

---
+++ src/test/java/com/brunoxavier/TesteClaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoxavier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteClaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
