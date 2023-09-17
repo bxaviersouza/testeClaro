2a1dcb0d216952c970c73d21acc118848a73fad1
Bruno Xavier <b.xavier.souza@gmail.com>
2023-09-17 15:30:55 -0300

    Criando operação de instanciação

--- src/main/java/com/brunoxavier/TesteClaroApplication.java
+++ src/main/java/com/brunoxavier/TesteClaroApplication.java
@@ -1,13 +1,32 @@
 package com.brunoxavier;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TesteClaroApplication {
+import com.brunoxavier.domain.Cliente;
+import com.brunoxavier.repositories.ClienteRepository;
 
+@SpringBootApplication
+public class TesteClaroApplication implements CommandLineRunner {
+
+	@Autowired
+	ClienteRepository clienteRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TesteClaroApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Cliente c1 = new Cliente(null, "Bruno Xavier");
+		Cliente c2 = new Cliente(null, "Caio Marques");
+		
+		clienteRepository.saveAll(Arrays.asList(c1, c2));
 	}
 
 }
